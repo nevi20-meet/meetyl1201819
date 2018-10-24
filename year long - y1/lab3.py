ad09e49250bbc14f71df03b4540a3471b5d34362
nevi20-meet <nevi20@meet.mit.edu>
2018-10-24 12:26:20 -0400

    i didnt finish helpppp

---
+++ year long - y1/lab3.py
@@ -0,0 +1,3 @@
+import turtle
+turtle.addshape("lollipop.gif")
+turtle.shape("lollipop.gif")
---
+++ year long - y1/turtleshapes.py
@@ -0,0 +1,34 @@
+import turtle
+''''
+turtle.pencolor("light blue")
+turtle.goto(200,0)
+turtle.pencolor("pink")
+turtle.goto(50,-150)
+turtle.pencolor("purple")
+turtle.goto(100,100)
+turtle.pencolor("light green")
+turtle.goto(150,-150)
+turtle.pencolor("blue")
+turtle.goto(0,0)
+'''
+'''
+turtle.fillcolor("light blue")
+turtle.begin_fill()
+turtle.goto(200,0)
+turtle.goto(200,-150)
+turtle.goto(100,-300)
+turtle.goto(0,-150)
+turtle.goto(0,0)
+turtle.end_fill()
+'''
+turtle.speed(0)
+for circle_1 in range(1000):
+	turtle.forward(300)
+	turtle.right(50)
+	turtle.forward(100)
+	turtle.right(90)
+	turtle.forward(50)
+	turtle.penup()
+	turtle.goto(0,0)
+	turtle.pendown()
+	turtle.right(circle_1)
